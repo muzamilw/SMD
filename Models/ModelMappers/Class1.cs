d6d3ac83b0bb4cadd258fb9437dc0c1ab78e58ca
khurram <khurram@innostark.com>
2015-12-01 10:33:04 +0500

    Gitignore file update

---
+++ Models/ModelMappers/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMD.Models.ModelMappers
+{
+    class Class1
+    {
+    }
+}
