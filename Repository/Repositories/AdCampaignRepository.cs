3a9383640ac4773fbfc2a71ebb097a46cb57aef6
iqramujahid <iqra@myprintcloud.com>
2015-12-03 14:59:33 +0500

    changes

---
+++ Repository/Repositories/AdCampaignRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Practices.Unity;
+using SMD.Interfaces.Repository;
+using SMD.Models.DomainModels;
+using SMD.Repository.BaseRepository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMD.Repository.Repositories
+{
+    public class AdCampaignRepository : BaseRepository<AdCampaign>, IAdCampaignRepository
+    {
+
+        public AdCampaignRepository(IUnityContainer container)
+            : base(container)
+        {
+
+        }
+
+        /// <summary>
+        /// Primary database set
+        /// </summary>
+        protected override IDbSet<AdCampaign> DbSet
+        {
+            get
+            {
+                return db.AdCampaigns;
+            }
+        }
+    }
+}
