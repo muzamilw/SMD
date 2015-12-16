2a23a1092174b067c8a1b2e0dbdfd27c4e5d3d4f
Baqer Naqvi <Link2naqvi@gmail.com>
2015-12-16 18:38:11 +0500

    Update Profile Question's Answer  API

---
+++ Models/RequestModels/UpdateProfileQuestionUserAnswerApiRequest.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Collections.Generic;
+
+namespace SMD.Models.RequestModels
+{
+    /// <summary>
+    /// Request Model For API
+    /// </summary>
+    public class UpdateProfileQuestionUserAnswerApiRequest
+    {
+        /// <summary>
+        /// Question Id 
+        /// </summary>
+        public int ProfileQuestionId { get; set; }
+
+        /// <summary>
+        /// Selected Answer list of Answers IDs
+        /// </summary>
+        public List<int> ProfileQuestionAnswerIds { get; set; }
+
+        /// <summary>
+        /// User Id
+        /// </summary>
+        public string UserId { get; set; }
+    }
+}
---
+++ SMD.Web/Areas/Api/Controllers/UpdateQuestionAnswerForApiController.cs
@@ -0,0 +1,43 @@
+﻿using SMD.Interfaces.Services;
+using SMD.Models.RequestModels;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+
+namespace SMD.MIS.Areas.Api.Controllers
+{
+    /// <summary>
+    /// Update Profile Question's Ans for APIs Controller 
+    /// </summary>
+    [Authorize]
+    public class UpdateQuestionAnswerForApiController : ApiController
+    {
+        #region Public
+        private readonly IProfileQuestionUserAnswerService profileQuestionUserAnswerService;
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Constuctor 
+        /// </summary>
+        public UpdateQuestionAnswerForApiController(IProfileQuestionUserAnswerService profileQuestionUserAnswerService)
+        {
+            this.profileQuestionUserAnswerService = profileQuestionUserAnswerService;
+        }
+
+        #endregion
+        #region Public
+
+        /// <summary>
+        /// Update Profile Question's Answer
+        /// </summary>
+        public string Post(UpdateProfileQuestionUserAnswerApiRequest request)
+        {
+            if (request == null || !ModelState.IsValid)
+            {
+                throw new HttpException((int)HttpStatusCode.BadRequest, "Invalid Request");
+            }
+            return profileQuestionUserAnswerService.UpdateProfileQuestionUserAnswer(request);
+        }
+        #endregion
+    }
+}
