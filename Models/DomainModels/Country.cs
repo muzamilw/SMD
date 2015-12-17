667f1f970267507874304bab73e7994cba692b4b
Baqer Naqvi <Link2naqvi@gmail.com>
2015-12-17 19:37:28 +0500

    I DO NOT KNOW NOTHING :|

--- Models/DomainModels/Country.cs
+++ Models/DomainModels/Country.cs
@@ -8,10 +8,13 @@ namespace SMD.Models.DomainModels
         public string CountryName { get; set; }
         public string CountryCode { get; set; }
 
+
+
         public virtual ICollection<ProfileQuestionGroup> ProfileQuestionGroups { get; set; }
         public virtual ICollection<AdCampaignTargetLocation> AdCampaignTargetLocations { get; set; }
         public virtual ICollection<SurveyQuestion> SurveyQuestions { get; set; }
         public virtual ICollection<SurveyQuestionTargetLocation> SurveyQuestionTargetLocations { get; set; }
         public virtual ICollection<City> Cities { get; set; }
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
