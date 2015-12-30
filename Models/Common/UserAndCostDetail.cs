0fa18e36da56b0954a5fda3b7739452de8bd5f52
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2015-12-30 15:48:34 +0500

    db changes

--- Models/Common/UserAndCostDetail.cs
+++ Models/Common/UserAndCostDetail.cs
@@ -20,5 +20,10 @@ namespace SMD.Models.Common
         public Nullable<double> ProfessionClausePrice { get; set; }
         public Nullable<double> EducationClausePrice { get; set; }
 
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Language { get; set; }
+        public string Industry { get; set; }
+        public string Education { get; set; }
     }
 }
--- SMD.Web/Areas/Api/Models/UserAndCostDetail.cs
+++ SMD.Web/Areas/Api/Models/UserAndCostDetail.cs
@@ -18,6 +18,10 @@ namespace SMD.MIS.Areas.Api.Models
         public Nullable<double> OtherClausePrice { get; set; }
         public Nullable<double> ProfessionClausePrice { get; set; }
         public Nullable<double> EducationClausePrice { get; set; }
-
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Language { get; set; }
+        public string Industry { get; set; }
+        public string Education { get; set; }
     }
 }
