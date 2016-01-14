396d05e2d799410b85923f142ac2b52067cd8130
iqramujahid <iqra@myprintcloud.com>
2016-01-14 11:07:20 +0500

    changes

--- Repository/Repositories/SurveyQuestionRepository.cs
+++ Repository/Repositories/SurveyQuestionRepository.cs
@@ -167,7 +167,8 @@ namespace SMD.Repository.Repositories
         {
             return db.GetAudienceCampaignAndSurveyCounts(request.ageFrom, request.ageTo, request.gender, request.countryIds, request.cityIds, request.languageIds, request.industryIds
                 , request.profileQuestionIds, request.profileAnswerIds, request.surveyQuestionIds, request.surveyAnswerIds,
-                request.countryIdsExcluded, request.cityIdsExcluded, request.languageIdsExcluded, request.industryIdsExcluded).FirstOrDefault();
+                request.countryIdsExcluded, request.cityIdsExcluded, request.languageIdsExcluded, request.industryIdsExcluded,
+                request.educationIds, request.educationIdsExcluded, request.profileQuestionIdsExcluded, request.surveyQuestionIdsExcluded).FirstOrDefault();
         }
         public UserBaseData getBaseData()
         {
