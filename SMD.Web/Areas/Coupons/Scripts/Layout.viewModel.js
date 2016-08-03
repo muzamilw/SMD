528751e58c4563225a249655092b940020534225
muhammad abuzar <shanabuzar@yahoo.com>
2016-08-03 19:19:19 +0500

    Branch dialog added

---
+++ SMD.Web/Areas/Coupons/Scripts/Layout.viewModel.js
@@ -0,0 +1,31 @@
+﻿define("Layout/Layout.viewModel",
+    ["jquery", "amplify", "ko", "Layout/Layout.dataService", "Layout/Layout.model", "common/confirmation.viewModel"],
+    function ($, amplify, ko, dataservice, model, confirmation) {
+        var ist = window.ist || {};
+        ist.Layout = {
+            viewModel: (function () {
+                var // The view 
+                   view,
+                      showBranchDialoge = function () {
+                          view.showBranchCategoryDialog();
+                      },
+                // Initialize the view model
+                 initialize = function (specifiedView) {
+                     view = specifiedView;
+                     ko.applyBindings(view.viewModel, view.bindingRoot);
+                     ko.applyBindings(view.viewModel, view.bindingPartial);
+                     
+                 };
+                return {
+
+                    initialize: initialize,
+                    showBranchDialoge: showBranchDialoge
+
+                };
+
+            })()
+        };
+
+        return ist.Layout.viewModel;
+
+    });
