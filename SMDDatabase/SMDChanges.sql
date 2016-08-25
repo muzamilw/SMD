e24fd87cfe9cbfe3909c60956a4638272fd4613c
teo-intl\muzzammil.waheed <muzamilw@hotmail.com>
2016-08-25 14:55:05 +0500

    scriptor

--- SMDDatabase/SMDChanges.sql
+++ SMDDatabase/SMDChanges.sql
@@ -8130,3 +8130,26 @@ GO
 ALTER TABLE dbo.SurveyQuestion SET (LOCK_ESCALATION = TABLE)
 GO
 COMMIT
+
+
+
+
+
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.AspNetUsers ADD
+	DevicePlatform int NULL
+GO
+ALTER TABLE dbo.AspNetUsers SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
