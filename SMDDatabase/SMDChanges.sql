fdcbeb18ef93052842930fd96ee943af6e8ed668
teo-intl\muzzammil.waheed <muzamilw@hotmail.com>
2016-08-26 15:50:29 +0500

    ok go

--- SMDDatabase/SMDChanges.sql
+++ SMDDatabase/SMDChanges.sql
@@ -8175,3 +8175,44 @@ GO
 ALTER TABLE dbo.Coupon SET (LOCK_ESCALATION = TABLE)
 GO
 COMMIT
+
+
+
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.adcampaign ADD
+	SubmissionDateTime datetime NULL
+GO
+ALTER TABLE dbo.adcampaign SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+
+
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.ProfileQuestion ADD
+	SubmissionDateTime datetime NULL
+GO
+ALTER TABLE dbo.ProfileQuestion SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
