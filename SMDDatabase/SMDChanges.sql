2a386daaaa6222580b03128a39d72b289ac5f14d
teo-intl\muzzammil.waheed <muzamilw@hotmail.com>
2016-09-01 18:24:37 +0500

    changes

--- SMDDatabase/SMDChanges.sql
+++ SMDDatabase/SMDChanges.sql
@@ -8629,3 +8629,26 @@ GO
 ALTER TABLE dbo.InvoiceDetail SET (LOCK_ESCALATION = TABLE)
 GO
 COMMIT
+
+
+
+/* To prevent any potential data loss issues, you should review this script in detail before running it outside the context of the database designer.*/
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.ProfileQuestionTargetCriteria ADD
+	AdCampaignID bigint NULL,
+	AdCampaignAnswer int NULL,
+	PQQuestionID int NULL
+GO
+ALTER TABLE dbo.ProfileQuestionTargetCriteria SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
