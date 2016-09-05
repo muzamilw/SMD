3e5631898c88b72de077990de23dc5f61de86f55
muhammad abuzar <shanabuzar@yahoo.com>
2016-09-06 03:19:40 +0500

    Added js file for knockout framework in analytics Dashboard

---
+++ SMD.Web/Areas/Api/Controllers/ActiveUserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SMD.MIS.Areas.Api.Controllers
+{
+    public class ActiveUserController : ApiController
+    {
+        // GET: api/ActiveUser
+        public IEnumerable<Int32> Get()
+        {
+            return new Int32[] { 1, 2 };
+        }
+
+        // GET: api/ActiveUser/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/ActiveUser
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/ActiveUser/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/ActiveUser/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ SMD.Web/Areas/SuperNova/Scripts/analytic.dataservice.js
@@ -0,0 +1,75 @@
+﻿/*
+    Data service module with ajax calls to the server
+*/
+define("analytic/analytic.dataservice", function () {
+
+    // Data service for forecast 
+    var dataService = (function () {
+        var // True if initialized
+            isInitialized = false,
+            // Initialize
+            initialize = function () {
+                if (!isInitialized) {
+
+                     
+                    amplify.request.define('getactiveuser', 'ajax', {
+                        url: '/Api/ApproveProfileQuestionAns',
+                        dataType: 'json',
+                        type: 'GET'
+                    });
+                    isInitialized = true;
+                }
+            },
+            // Search Ad Campaigns
+            getPQForApproval = function (params, callbacks) {
+                initialize();
+                return amplify.request({
+                    resourceId: 'getPQForApproval',
+                    success: callbacks.success,
+                    error: callbacks.error,
+                    data: params
+                });
+            },
+
+
+
+             // Search Ad Campaigns
+            sendApprovalRejectionEmail = function (params, callbacks) {
+                initialize();
+                return amplify.request({
+                    resourceId: 'sendApprovalRejectionEmail',
+                    success: callbacks.success,
+                    error: callbacks.error,
+                    data: params
+                });
+            },
+            // Save Ad Campaign edit
+            savePq = function (params, callbacks) {
+                initialize();
+                return amplify.request({
+                    resourceId: 'savePq',
+                    success: callbacks.success,
+                    error: callbacks.error,
+                    data: params
+                });
+            },
+        // Get Profile Questions Answer On edit 
+        getPqAnswer = function (params, callbacks) {
+            initialize();
+            return amplify.request({
+                resourceId: 'getPqAnswer',
+                success: callbacks.success,
+                error: callbacks.error,
+                data: params
+            });
+        };
+
+        return {
+            savePq: savePq,
+            getPQForApproval: getPQForApproval,
+            sendApprovalRejectionEmail: sendApprovalRejectionEmail,
+            getPqAnswer: getPqAnswer
+        };
+    })();
+    return dataService;
+});
