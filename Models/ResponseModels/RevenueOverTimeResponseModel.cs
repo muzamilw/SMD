ce25b1b055db27f01ab5f5f87077fa7fd8af714e
muhammad abuzar <shanabuzar@yahoo.com>
2016-09-08 13:05:47 +0500

    analytic dashboard

---
+++ Models/ResponseModels/RevenueOverTimeResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMD.Models.ResponseModels
+{
+    public class RevenueOverTimeResponseModel
+    {
+
+        public Int32 AggregateRevenue { get; set; }
+        public String Granularity { get; set; }
+
+    }
+}
