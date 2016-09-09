f05554b30a96448474699db4f13b16793cb43a44
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2016-09-09 15:46:56 +0500

    completed working for getting company address

---
+++ SMD.Web/Areas/Api/Controllers/GetCompanyController.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using SMD.Interfaces.Services;
+using SMD.MIS.Areas.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SMD.MIS.Areas.Api.Controllers
+{
+    public class GetCompanyController : ApiController
+    {
+        #region Attributes 
+        private readonly ICompanyService _companyService;
+        #endregion
+
+        #region Constructor
+        public GetCompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        
+        }
+        #endregion
+
+        #region methods
+        public CompanybillingAddress Get(int companyId)
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<SMD.Models.DomainModels.Company, CompanybillingAddress>());
+            var obj = _companyService.GetCompanyById(companyId);
+
+            return Mapper.Map<SMD.Models.DomainModels.Company, CompanybillingAddress>(obj);
+        }
+
+        #endregion
+    }
+}
