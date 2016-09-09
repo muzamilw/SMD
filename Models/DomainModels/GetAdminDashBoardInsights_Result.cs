67bd3a8f0548126b8a02a9ab0de7e7f3c90a3ab5
teo-intl\muzzammil.waheed <muzamilw@hotmail.com>
2016-09-09 12:18:47 +0500

    DB changes & bounce game

---
+++ Models/DomainModels/GetAdminDashBoardInsights_Result.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMD.Models.DomainModels
+{
+    public partial class GetAdminDashBoardInsights_Result
+    {
+        public string rectype { get; set; }
+        public string pMonth { get; set; }
+        public Nullable<int> us { get; set; }
+        public Nullable<int> uk { get; set; }
+        public Nullable<int> ca { get; set; }
+        public Nullable<int> au { get; set; }
+        public Nullable<int> ae { get; set; }
+    }
+}
