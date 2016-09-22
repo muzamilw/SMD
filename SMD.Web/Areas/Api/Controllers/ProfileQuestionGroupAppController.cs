d64a93ad396ebc69b36804a4a2dd600f378fe1a7
Muhammad Saqib Ali <saqibali@myprintcloud.com>
2016-09-22 18:54:17 +0500

    changes for Approval Dashboard

---
+++ SMD.Web/Areas/Api/Controllers/ProfileQuestionGroupAppController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using SMD.Interfaces.Services;
+using SMD.MIS.Areas.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SMD.MIS.Areas.Api.Controllers
+{
+    public class ProfileQuestionGroupAppController : ApiController
+    {
+        #region Attributes 
+        private readonly IProfileQuestionService _profileQuestionService;
+        #endregion
+
+        #region Constructor
+        public ProfileQuestionGroupAppController(IProfileQuestionService _profileQuestionService)
+        {
+          this._profileQuestionService = _profileQuestionService;
+        
+        }
+        #endregion
+
+        #region Methods
+        public ProfileQuestionGroupApp Get(int id)
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<SMD.Models.DomainModels.ProfileQuestionGroup, ProfileQuestionGroupApp>());
+            var obj = _profileQuestionService.GetPQGroupById(id);
+            return Mapper.Map<SMD.Models.DomainModels.ProfileQuestionGroup, ProfileQuestionGroupApp>(obj);
+        }
+
+        #endregion
+    }
+}
