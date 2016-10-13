b1f326c85a1e14f1952e2938b682c08f1a89d98f
teo-intl\muzzammil.waheed <muzamilw@hotmail.com>
2016-10-13 19:42:07 +0500

    coupon changes.

--- SMD.Web/Areas/Coupons/Scripts/Coupons.viewModel.js
+++ SMD.Web/Areas/Coupons/Scripts/Coupons.viewModel.js
@@ -163,24 +163,24 @@ define("Coupons/Coupons.viewModel",
                                     var set = parseInt((couponCategories().length / 3));
 
                                     //For 1s Column
-                                    for (var i = 0; i < set; i++) {
+                                    for (var i = 0; i < couponCategories().length; i++) {
                                         couponCategoriesCol1.push(couponCategories()[i]);
                                     }
                                     couponCategoriesCol1.valueHasMutated();
-                                    //For 2nd Column
-                                    if (couponCategories().length > set + 1) {
-                                        for (var j = set + 1; j < set*2 + 1; j++) {
-                                            couponCategoriesCol2.push(couponCategories()[j]);
-                                        }
-                                        couponCategoriesCol2.valueHasMutated();
-                                    }
-                                    //For 3rd Column
-                                    if (couponCategories().length >= set * 2 + 1) {
-                                        for (var k = set * 2 + 1; k < couponCategories().length; k++) {
-                                            couponCategoriesCol3.push(couponCategories()[k]);
-                                        }
-                                        couponCategoriesCol2.valueHasMutated();
-                                    }
+                                    ////For 2nd Column
+                                    //if (couponCategories().length > set + 1) {
+                                    //    for (var j = set + 1; j < set*2 + 1; j++) {
+                                    //        couponCategoriesCol2.push(couponCategories()[j]);
+                                    //    }
+                                    //    couponCategoriesCol2.valueHasMutated();
+                                    //}
+                                    ////For 3rd Column
+                                    //if (couponCategories().length >= set * 2 + 1) {
+                                    //    for (var k = set * 2 + 1; k < couponCategories().length; k++) {
+                                    //        couponCategoriesCol3.push(couponCategories()[k]);
+                                    //    }
+                                    //    couponCategoriesCol2.valueHasMutated();
+                                    //}
 
                                 }
 
