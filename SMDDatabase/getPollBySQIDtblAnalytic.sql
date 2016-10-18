7e141bd039baf3131aded41b6bfb2cc5b771cae1
muhammad abuzar <shanabuzar@yahoo.com>
2016-10-18 16:04:32 +0500

    SP added

---
+++ SMDDatabase/getPollBySQIDtblAnalytic.sql
@@ -0,0 +1,58 @@
+﻿GO
+/****** Object:  StoredProcedure [dbo].[getAdsCampaignByCampaignIdRatioAnalytic]    Script Date: 10/18/2016 1:26:40 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+Create PROCEDURE [dbo].[getPollBySQIDtblAnalytic] (
+@Id INT  -- 1 for Viewed, 2 for Conversions or 3 for Skipped
+)
+AS
+BEGIN
+-- SET NOCOUNT ON added to prevent extra result sets from
+-- interfering with SELECT statements.
+SET NOCOUNT ON;
+DECLARE @dateFrom DATE = getdate()-30;
+
+	BEGIN
+	 Select top 1 @dateFrom= [ResoponseDateTime] from SurveyQuestionResponse where SQID = @Id
+	END
+
+
+		Select 1 ordr, 'Delivered' label, count(sqr.SQID) 'All time' ,
+			 (Select count(sqr.SQID) from SurveyQuestionResponse sqr
+			  where sqr.ResoponseDateTime >= getdate() - 30 and sqr.ResoponseDateTime <= getdate() and sqr.ResponseType = 1 and sqr.SQID = @Id) '30 days'
+		from SurveyQuestionResponse sqr
+		where sqr.ResoponseDateTime >= @dateFrom and sqr.ResoponseDateTime <= getdate() and sqr.ResponseType = 1 and sqr.SQID = @Id
+		
+		union
+		
+		Select 2 ordr, 'Skipped' label, count(sqr.SQID) 'All time' ,
+			 (Select  count(sqr.SQID) from SurveyQuestionResponse sqr
+			  where sqr.ResoponseDateTime >= getdate() - 30 and sqr.ResoponseDateTime <= getdate() and sqr.ResponseType = 4 and sqr.SQID = @Id) '30 days'
+		from SurveyQuestionResponse sqr
+		where sqr.ResoponseDateTime >= @dateFrom and sqr.ResoponseDateTime <= getdate() and sqr.ResponseType = 4 and sqr.SQID = @Id
+
+		union
+		Select 3 ordr, 'Answered' label, count(sqr.SQID) 'All time' ,
+			 (Select  count(sqr.SQID) from SurveyQuestionResponse sqr
+			  where sqr.ResoponseDateTime >= getdate() - 30 and sqr.ResoponseDateTime <= getdate() and sqr.ResponseType = 3 and sqr.SQID = @Id) '30 days'
+		from SurveyQuestionResponse sqr
+		where sqr.ResoponseDateTime >= @dateFrom and sqr.ResoponseDateTime <= getdate() and sqr.ResponseType = 3 and sqr.SQID = @Id
+			
+		union
+		Select 6 ordr, 'Cost ($)' label, isnull(sum(t.DebitAmount)/100, 0) 'All time' , 
+			(Select isnull(sum(t.DebitAmount)/100, 0) from [Transaction] t
+			where t.TransactionDate >= getdate() - 30 and t.TransactionDate <= getdate() and t.SQID = @Id) '30 days'
+		from [Transaction] t
+		where t.TransactionDate >= @dateFrom and t.TransactionDate <= getdate() AND t.SQID = @Id
+				
+END
+  
+--EXEC [getPollBySQIDtblAnalytic] 12
+
