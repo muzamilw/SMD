87fa904ed2497e710b7cae3433f936bd2778eb0b
muhammad abuzar <shanabuzar@yahoo.com>
2016-11-18 23:16:39 +0500

    campaign form

---
+++ Implementation/Services/AdCampaignResponseService.cs
@@ -0,0 +1,15 @@
+﻿using SMD.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMD.Implementation.Services
+{
+    public class AdCampaignResponseService : IAdCampaignResponseService
+    {
+
+
+    }
+}
---
+++ SMDDatabase/getAdsCampaignByIdFormAnalytic.sql
@@ -0,0 +1,90 @@
+﻿
+GO
+/****** Object:  StoredProcedure [dbo].[getAdsCampaignByCampaignIdRatioAnalytic]    Script Date: 11/15/2016 11:03:46 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+create PROCEDURE [dbo].[getCampaignByIdQQFormAnalytic] (
+@Id INT,
+@coice INT,
+@gender INT,  -- 0 for all, 1 for male and 2 for female
+@ageRange INT, -- 0 for All , 1 for 10-20 , 2 for 20-30, 3 for 30-40 , 4 for 40-50, 5 for 50-60, 6 for 60-70, 7 for 70-80, 8 for 80-90, 9 for 90+
+@Profession nvarchar(250),	
+@city nvarchar(250)
+)
+AS
+BEGIN
+-- SET NOCOUNT ON added to prevent extra result sets from
+-- interfering with SELECT statements.
+SET NOCOUNT ON;
+DECLARE @AgedateFrom DATE =  DATEADD(YYYY,-100,getdate()) ;
+DECLARE @AgedateTo DATE = getdate();
+ 
+
+IF @ageRange = 1
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-20,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-10,getdate()) ;
+	END
+IF @ageRange = 2
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-30,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-20,getdate()) ;
+	END
+	IF @ageRange = 3
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-40,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-30,getdate()) ;
+	END
+	IF @ageRange = 4
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-50,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-40,getdate()) ;
+	END
+	IF @ageRange = 5
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-60,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-50,getdate()) ;
+	END
+	IF @ageRange = 6
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-70,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-60,getdate()) ;
+	END
+	IF @ageRange = 7
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-80,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-70,getdate()) ;
+	END
+	IF @ageRange = 8
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-90,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-80,getdate()) ;
+	END
+	IF @ageRange = 9
+	BEGIN
+		set @AgedateFrom = DATEADD(YYYY,-500,getdate()) ;
+		set @AgedateTo = DATEADD(YYYY,-90,getdate()) ;
+	END
+	select count(*) from AdCampaignResponse ac
+	inner join AspNetUsers usr on ac.UserID = usr.Id
+	where ac.CampaignID = @Id and ac.ResponseType = 3 
+	and (ac.UserSelection = @coice OR @coice = 0) and (usr.Gender = @gender OR @gender = 0)
+	and (usr.Jobtitle = @Profession OR @Profession = 'All') and (ac.UserLocationCity = @city OR @city = 'All')
+	and usr.DOB > = @AgedateFrom and usr.DOB < @AgedateTo
+		
+		
+		
+	
+END
+
+ 
+--EXEC [getCampaignByIdQQFormAnalytic] 20369, 0, 0, 0, 'All', 'All'
+
+
