4f8432e28497efe88d230d8b7b76564ff84060b1
teo-intl\muzzammil.waheed <muzamilw@hotmail.com>
2017-01-06 17:20:51 +0500

    campaign submission emails formats.

---
+++ SMD.Web/Emails/Picture_poll_Submission.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SMD.MIS.Views
+{
+    public partial class picture_poll_submission : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
