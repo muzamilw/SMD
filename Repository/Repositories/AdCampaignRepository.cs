1f401fa611fb22f1d6a3809888b2c3c4c4f6624c
shanabuzar <shanabuzar@yahoo.com>
2017-01-09 16:21:30 +0500

    done

--- Repository/Repositories/AdCampaignRepository.cs
+++ Repository/Repositories/AdCampaignRepository.cs
@@ -217,7 +217,7 @@ namespace SMD.Repository.Repositories
             if (results.Count() > 0)
             {
                 var firstrec = results.First();
-                rowCount = results.Count();
+                rowCount = firstrec.TotalItems.Value;
             }
             else
                 rowCount = 0;
