0533ad2350b62f093e0e8138dff09a51ea50d805
muhammad abuzar <shanabuzar@yahoo.com>
2017-01-18 23:43:39 +0500

    Video Grid changes

--- SMDDatabase/getCampaignROItblAnalytic.sql
+++ SMDDatabase/getCampaignROItblAnalytic.sql
@@ -1,6 +1,6 @@
 ﻿USE [SMDv2]
 GO
-/****** Object:  StoredProcedure [dbo].[getCampaignROItblAnalytic]    Script Date: 11/25/2016 4:47:51 PM ******/
+/****** Object:  StoredProcedure [dbo].[getCampaignROItblAnalytic]    Script Date: 1/18/2017 11:26:08 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -24,27 +24,28 @@ DECLARE @dateFrom DATE = getdate()-30, @openAds float = 1;
 	 Select top 1 @dateFrom= [CreatedDateTime] from AdCampaignResponse where CampaignID = @Id
 	 Select  @openAds= count(acr.CampaignID) from AdCampaignResponse acr where acr.ResponseType = 1 and acr.CampaignID = @Id
 	END
-		Select 1 ordr, 'Watched in full & answered survey' label1, count(acr.CampaignID) Stats1 , 'Campaign Cost to date' label2, (Select isnull(ROUND(sum(t.DebitAmount)/100,2), 0) 
-																														from [Transaction] t
-																														where t.AdCampaignID = @Id) stats2, 'Impressions to Click Thru' label3, 
-																													(Select (case when @openAds > 0 then CAST((count(acr.CampaignID)/(@openAds))*100 as int) else count(acr.CampaignID) end )  			 
-																													from AdCampaignResponse acr where acr.ResponseType = 2 and acr.CampaignID = @Id ) Stats3 		 
-		from AdCampaignResponse acr
-		where acr.ResponseType = 3 and acr.CampaignID = @Id
+		Select 1 ordr, 'Campaign Cost to date' label, isnull(ROUND(sum(t.DebitAmount)/100,2), 0) stats
+		from [Transaction] t
+		where t.AdCampaignID = @Id		
+		union	
+		
+		Select 2 ordr, 'Rewarded to audiences' label2, ROUND(isnull(sum(t.CreditAmount)/100, 0),2)   Stats
+		FROM [Transaction] t
+		inner join Account a on a.AccountId = t.AccountID
+		where t.AdCampaignID = @Id and a.CompanyId != 466	
 		union
-		Select 2 ordr, 'Click Thru' label1, count(acr.CampaignID) Stats1, 'Rewarded to audiences' label2, 
-								(Select ROUND(isnull(sum(t.CreditAmount)/100, 0),2)   
-								FROM [Transaction] t
-								inner join Account a on a.AccountId = t.AccountID
-								where t.AdCampaignID = @Id and a.CompanyId != 466) 	stats2, 'Impressions to Quiz Results' label3, 
-											(Select (case when @openAds > 0 then CAST((count(acr.CampaignID)/(@openAds))*100 AS int) else count(acr.CampaignID) end )  			 
-											from AdCampaignResponse acr
-											where acr.ResponseType = 3 and acr.CampaignID = @Id	) Stats3		 
+	
+		Select 3 ordr, 'Average ad click' label, isnull(ROUND((sum(t.DebitAmount)/count(t.DebitAmount))/100, 2), 0) Stats
+		from [Transaction] t
+		where t.AdCampaignID = @Id
+		union
+																								
+		Select 4 ordr, 	 'Impressions to Click Thru' label, (case when @openAds > 0 then CAST((count(acr.CampaignID)/(@openAds))*100 as int) else count(acr.CampaignID) end )  	Stats		 
+		from AdCampaignResponse acr where acr.ResponseType = 2 and acr.CampaignID = @Id 																												
+		union
+		Select 5 ordr,	'Impressions to Quiz Results' label,  (case when @openAds > 0 then CAST((count(acr.CampaignID)/(@openAds))*100 AS int) else count(acr.CampaignID) end )  Stats			 
 		from AdCampaignResponse acr
-		where acr.ResponseType = 2 and acr.CampaignID = @Id
-		
-		
-		
+		where acr.ResponseType = 3 and acr.CampaignID = @Id	 
 		
 			
 END
