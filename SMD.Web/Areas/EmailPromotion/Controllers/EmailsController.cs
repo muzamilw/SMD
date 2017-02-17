7d5d6893e890dbd9aded7ce33bbefbd03782b325
shanabuzar <shanabuzar@yahoo.com>
2017-02-17 09:31:15 +0500

    working on Email promotion

---
+++ SMD.Web/Areas/EmailPromotion/Controllers/EmailsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMD.MIS.Areas.EmailPromotion.Controllers
+{
+    public class EmailsController : Controller
+    {
+        // GET: EmailPromotion/Emails
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
